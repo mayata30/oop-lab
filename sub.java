1a83a2e113762f5034e96ac0896c3b658b4913cc
mayata30 <mayaappuz36@gmail.com>
2025-04-30 11:20:23 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,16 @@
+package arithmetic;
+
+public class sub implements operations
+{
+	private double num1;
+	private double num2;
+	public sub(double num1,double num2)
+	{
+		this.num1=num1;
+		this.num2=num2;
+	}
+	public double oprns()
+	{
+		return num1-num2;
+	}
+}
